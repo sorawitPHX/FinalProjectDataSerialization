31f0e9e13f4a9c86e99abde90233aa13f9679623
sorawitPHX <114576982+sorawitPHX@users.noreply.github.com>
2024-09-27 05:30:12 +0700

    ลบ node_module

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Ngamnij Arch-int' });
+  res.render('index', { title: 'Ngamnij Arch-int', activePage: 'index' });
 });
 
 module.exports = router;
