2bba140ec468472e7b6679c26dc14ae6af6397ae
PloyU <137448173+ArayaPloy@users.noreply.github.com>
2024-10-07 10:10:38 +0700

    แก้ปุ่มเข้าสู่ระบบ

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require('express'); // ใช้ Express.js เพื่อสร้างเซิร์ฟเวอร์ HTTP และจัดการการร้องขอจากฝั่งผู้ใช้
+const bcrypt = require('bcrypt'); // ใช้สำหรับการเข้ารหัสรหัสผ่านเพื่อให้รหัสผ่านที่เก็บไว้ในระบบปลอดภัย
 const session = require('express-session'); // ใช้ session ในการจัดการ session ของผู้ใช้
 const router = express.Router();
 const User = require('../models/User')
 
 router.use(express.json()); // เพิ่ม middleware นี้เพื่อตรวจสอบการรับ JSON
 
-// ตัวอย่างข้อมูลผู้ใช้
+// ตัวอย่างข้อมูลผู้ใช้ เมื่อผู้ใช้ส่งอีเมลและรหัสผ่านผ่านการร้องขอแบบ POST มายัง /auth/login
 const users = [
     { username: 'user1', email: 'user1@example.com', password: bcrypt.hashSync('password1', 10) },
     { username: 'user2', email: 'user2@example.com', password: bcrypt.hashSync('password2', 10) }
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
     if (user && await bcrypt.compare(password, user.password)) {
         req.session.user = user;
 
-        // หากเลือก "Remember me"
+        // หากเลือก "Remember me" ระบบจะตั้งค่า session cookie ให้ใช้งานได้ 30 วัน
         if (rememberMe) {
             req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // จดจำ 30 วัน
         } else {
